15c3c0a4dd22bab7fd924283f166bd17dacb4e4c
Alijoshaghani <136457347+Ali-joshaghani@users.noreply.github.com>
2023-06-18 13:46:24 +0330

    Create Four_X's_and_four_Y's.py

---
+++ Receive linear equations and send the Four X's and four Y's/Four_X's_and_four_Y's.py
@@ -0,0 +1,15 @@
+# y = mx + b
+m = float(input("enter m = "))
+b = float(input("enter b = "))
+x1 = float(1)
+x2 = float(2)
+x3 = float(3)
+x4 = float(4)
+y1 = m * x1  + b
+y2 = m * x2  + b
+y3 = m * x3  + b
+y4 = m * x4  + b
+x = [x1,x2,x3,x4]
+y = [y1,y2,y3,y4]
+print("X = ",x)
+print("Y = ",y)
